d12970a16d2d216fb12a59ab25e8ea4d0aa13b9c
Victor Sand <victor.sand@gmail.com>
2016-03-05 17:08:16 +0000

    switch messages using PROGMEM table

--- arduino/scrolling_message/scrolling_message.ino
+++ arduino/scrolling_message/scrolling_message.ino
@@ -1,5 +1,4 @@
 #include <avr/pgmspace.h>
-
 #include <Adafruit_GFX.h>
 #include <RGBmatrixPanel.h>
 #include <elapsedMillis.h>
@@ -18,14 +17,36 @@ const int TEXT_SIZE = 1;
 const int TEXT_WIDTH = 6; // width of font + one whitespace
 const int DRAW_DELAY = 100; // milliseconds
 
-const char message[] PROGMEM = "Scrolling message with border;
-int messageLength = strlen_P(message);
-char c;
+const char message_0[] PROGMEM = "Hello";
+const char message_1[] PROGMEM = "OK!";
+const char message_2[] PROGMEM = "Some really long string that would not fit in SRAM";
 
-int messageOffsetX = WIDTH;
-int minOffsetX = messageLength * TEXT_WIDTH * -1;
+const char* const message_table[] PROGMEM = {
+  message_0,
+  message_1,
+  message_2
+};
 
-elapsedMillis timeElapsed = 0;
+const int strlen_table[] = {
+  strlen_P(message_0),
+  strlen_P(message_1),
+  strlen_P(message_2)
+};
+
+elapsedMillis time_elapsed = 0;
+
+int message_index;
+char character;
+
+int message_length;
+int message_offset_x;
+int min_offset_x;
+
+void selectMessage(int index) {
+  message_index = index;
+  message_length = strlen_table[message_index];
+  min_offset_x = message_length * TEXT_WIDTH * -1;
+}
 
 void drawBorder(int color) {
   matrix.drawLine(0, 0, WIDTH-1, 0, color);
@@ -37,9 +58,10 @@ void drawBorder(int color) {
 void printMessage(int offsetX, int color) {
   matrix.setTextColor(color);
   matrix.setCursor(offsetX, 4);
-  for (int i=0; i<messageLength; i++) {
-    c = pgm_read_byte_near(message + i);
-    matrix.print(c);
+  PGM_P prog_ptr = (prog_char*)pgm_read_word(&message_table[message_index]);
+  for (int i=0; i<message_length; i++) {
+    character = (char)pgm_read_byte(&prog_ptr[i]);
+    matrix.print(character);
   }
 }
 
@@ -47,16 +69,24 @@ void setup() {
   matrix.begin();
   matrix.setTextWrap(false);
   matrix.setTextSize(TEXT_SIZE);
+  
+}
+
+void updateScroll() {
+  if (time_elapsed > DRAW_DELAY) {
+    message_offset_x--;
+    if (message_offset_x < min_offset_x) {
+      message_offset_x = WIDTH;
+    }
+    time_elapsed = 0;
+  }
 }
 
 void loop() {
+  selectMessage(0);
   matrix.fillScreen(0);
-  if (timeElapsed > DRAW_DELAY) {
-      messageOffsetX--;
-      if (messageOffsetX < minOffsetX) messageOffsetX = WIDTH;
-      timeElapsed = 0;
-  }
-  printMessage(messageOffsetX, matrix.Color333(5, 5, 5));
-  drawBorder(matrix.Color333(5, 0, 0));
+  updateScroll();
+  printMessage(message_offset_x, matrix.Color333(5, 5, 5));
+  drawBorder(matrix.Color333(7, 0, 0));
   matrix.swapBuffers(false);
 }
