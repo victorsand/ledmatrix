99776fb86555c06e91179513c58837a57d80b66c
Victor Sand <victor.sand@gmail.com>
2016-03-28 21:16:31 +0200

    wip http reqs

--- dashboard/MainController.js
+++ dashboard/MainController.js
@@ -1,5 +1,5 @@
-app.controller("MainController", ["$scope", function($scope) {
-	$scope.title = "LED Matrix Dashboard";
+app.controller("MainController", function($scope, $http) {
+
 	$scope.recurringMessages = [
 		{
 			id: "abc123",
@@ -18,6 +18,82 @@ app.controller("MainController", ["$scope", function($scope) {
 			message: "Help, stuck in bathroom",
 			messageColor: "707",
 			borderColor: "070"
-		},
+		}
 	];
-}]);
+
+	$scope.refreshMessages = function() {
+		$http({
+			method: "GET",
+			url: "http://httpbin.org/get"
+		}).then(function success(response) {
+			console.log("response", response);
+		}, function error(response) {
+			console.error("error", response);
+		});
+	};
+
+	$scope.addRecurringMessage = function(message) {
+		$http({
+			method: "POST",
+			url: "http://httpbin.org/post",
+			data: {
+				message: message,
+				messageColor: "777",
+				borderColor: "007"
+			}
+		}).then(function success() {
+			console.log("Added recurring message")
+		}, function error(response) {
+			console.error(reponse);
+		});
+	};
+
+	$scope.addTemporaryMessage = function(message) {
+		$http({
+			method: "POST",
+			url: "http://httpbin.org/post",
+			data: {
+				message: message,
+				messageColor: "777",
+				borderColor: "007"
+			}
+		}).then(function success() {
+			console.log("Added temporary message")
+		}, function error(response) {
+			console.error(reponse);
+		});
+	};
+
+	$scope.removeMessage = function(index) {
+		var message = $scope.recurringMessages[index];
+		console.log("Removing message", message.message);
+		$http({
+			method: "POST",
+			url: "http://httpbin.org/post",
+			data: {
+				id: message.id,
+			}
+		}).then(function success() {
+			console.log("Removed message");
+		}, function error(response) {
+			console.error(reponse);
+		});
+	};
+
+	$scope.clearMessages = function() {
+		console.log("Clearing all messages");
+		$http({
+			method: "POST",
+			url: "http://httpbin.org/post",
+		}).then(function success() {
+			console.log("Cleared messages");
+		}, function error(response) {
+			console.error(reponse);
+		});
+	};
+
+	$scope.refreshMessages();
+	$scope.addMessage();
+	$scope.removeMessage(0);
+
+});
